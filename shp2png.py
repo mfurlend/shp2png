50d4508488679c4c856d6439b57bc84d560c6897
Mike Furlender <furlender@weatherbell.com>
2014-09-25 12:45:47 -0400

    removed description from shp2png.py, moved to README.md

--- shp2png.py
+++ shp2png.py
@@ -1,33 +1,3 @@
-"""
-shp2png.py - Creates a png image from a shapefile.
-By default it will draw all polygons in the shapefile.
-You can specify which polygon you want to draw by providing
-an attribute filter. Attribute filters can be chained.
-
-The syntax for an attribute filter is as followed:
-
-python shp2png.py <SHAPEFILE NAME> -f <ATTRIBUTE NAME> "<ATTRIBUTE VALUE>" -f <SECOND ATTRIBUTE NAME> "<SECOND ATTRIBUTE VALUE>" -o <OUTPUT NAME>
-
-
-You can batch-generate multiple images by specifying a "group by" constraint.
-"Group by" constraints look like attribute filters, 
-but are surrounded by [square brackets] and do not have attribute values.
-
-The syntax for a "group by" constraint is as followed:
-python shp2png.py <SHAPEFILE NAME> -f [<ATTRIBUTE NAME>] -o <OUTPUT PREFIX>
-
-The polygon fill and stroke colors are white by default. 
-You can override the colors with the "--fill" and "--stroke" options respectively.
-
-The default image width is 4333 pixles. 
-You can override this with the --width option.
-
-author: mfurlend@gmail.com
-date: 20140910
-
-This script requires the Python Imaging Library.
-"""
-
 import shapefile, sys
 import Image, ImageDraw
 import argparse
